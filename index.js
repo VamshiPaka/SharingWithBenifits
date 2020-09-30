950c30f11a79dfb6e8d5e07823a3280aa201b20b
VamshiPaka <vamshi.paka@gmail.com>
2020-09-30 18:53:29 +0530

    MongoDB Connected

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
 const app = express();
 const port = 3000;
+
+//DB Connection
+mongoose.connect(process.env.DB_Connection, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+	console.log('connected to DB')
+);
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Sharing with benifits');
